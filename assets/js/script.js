dc9edcbc2e3274cc576d21f1e44dbff578d2af59
Teplin Dev <teplin.dev@gmail.com>
2021-12-01 22:29:54 +0400

    asdasd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,26 @@
 $(document).ready(function () {
 
 
-    let logo        = $(".header-logo"),
-        menu        = $(".header-menu"),
-        header      = $(".header-wrapper"),
-        text        = $(".video-animation").find(".text-block"),
-        windowWidth,
-        borderWidth;
+  let logo        = $(".header-logo"),
+      menu        = $(".header-menu"),
+      header      = $(".header-wrapper"),
+      text        = $(".video-animation").find(".text-block"),
+      windowWidth,
+      borderWidth;
 
-    scrollAction();
-    mediaActions();
-    window.onscroll = function () {
-        scrollAction();
-    };
-    window.onresize = function () {
-        mediaActions();
-    };
-
-
-
-  $('.screen-section').each(function() {
-    var target = $(this);
-
-
-    $(window).scroll(function() {
-      var scrollTop = $(window).scrollTop();
-      var elOffTop = $('.screen-section').offset().top;
-      var offset = target[0].getBoundingClientRect().top - target[0].offsetParent.getBoundingClientRect().top;
-      const top = window.pageYOffset + window.innerHeight - offset;
-      // console.log($('.scroll-item')[0].getBoundingClientRect())
-    })
-  })
-
+      
   var defaultOptions = {
     container: document.body,
     panelSelector: '*[data-section]',
     directionThreshold: 100,
     delay: 0,
-    duration: 500,
+    duration: 800,
     easing: 'ease'
   };
 
   let panel = new PanelSnap(defaultOptions);
   $('.scroll-btn').click(function() {
-    let next = $(panel.activePanel).data('i') + 1;
+    let next = $(panel.activePanel).data('section') + 1;
     let pane = panel.panelList.find(el=> {
       return $(el).data('section') == next
     })
@@ -51,6 +28,22 @@ $(document).ready(function () {
     console.log(p)
     panel.snapToPanel(p)
   })
+
+
+
+
+  scrollAction();
+  mediaActions();
+  window.onscroll = function () {
+      scrollAction();
+  };
+  window.onresize = function () {
+      mediaActions();
+  };
+
+
+
+
 
   $(".video-animation").each(function () {
     var caseTriggered = false;
@@ -137,48 +130,104 @@ $(document).ready(function () {
     // text.css({ "max-width": `${windowWidth - (borderWidth * 2)}px` });
   }
   function scrollAction() {
+
+
+    // header change colors
     let lo = header.offset();
     let y = lo.top - window.scrollY + header.height() / 2;
     let x = lo.left + logo.width() / 2;
-    header.addClass("sink");
-    let element_from_point = elementsFromPoint(x, y).find((el) => {
-      return $(el).data("l");
-    });
+    // header.addClass("sink");
+    // let element_from_point = elementsFromPoint(x, y).find((el) => {
+    //   return $(el).data("l");
+    // });
     // console.log(element_from_point);
-    header.removeClass("sink");
+    // header.removeClass("sink");
 
-    if (element_from_point) {
-      let l = element_from_point.dataset.l;
-      if (l !== undefined) {
-        if (l == 1) {
-          if (element_from_point.tagName == "VIDEO") {
-            if (
-              !$(element_from_point)
-                .closest(".video-animation")
-                .hasClass("animation-done")
-            ) {
-              logo.removeClass("white");
-              menu.removeClass("white");
+    // if (element_from_point) {
+    //   let l = element_from_point.dataset.l;
+    //   if (l !== undefined) {
+    //     if (l == 1) {
+    //       if (element_from_point.tagName == "VIDEO") {
+    //         if (
+    //           !$(element_from_point)
+    //             .closest(".video-animation")
+    //             .hasClass("animation-done")
+    //         ) {
+    //           logo.removeClass("white");
+    //           menu.removeClass("white");
+    //         } else {
+    //           logo.addClass("white");
+    //           menu.addClass("white");
+    //         }
+    //       } else {
+    //         logo.addClass("white");
+    //         menu.addClass("white");
+    //       }
+    //     } else {
+    //       logo.removeClass("white");
+    //       menu.removeClass("white");
+    //     }
+    //   } else {
+    //     logo.removeClass("white");
+    //     menu.removeClass("white");
+    //   }
+    // } else {
+    //   logo.removeClass("white");
+    //   menu.removeClass("white");
+    // }
+
+
+
+
+    $('._color').each(function() {
+      let $object = $(this);
+      let x       = $object[0].getBoundingClientRect().x;
+      let y       = $object[0].getBoundingClientRect().y;
+
+      let element_from_point = elementsFromPoint(x, y).find((el) => {
+        return $(el).data("l");
+      });
+      if (element_from_point) {
+        let l = element_from_point.dataset.l;
+        if (l !== undefined) {
+          if (l == 1) {
+            if (element_from_point.tagName == "VIDEO") {
+              if (
+                !$(element_from_point)
+                  .closest(".video-animation")
+                  .hasClass("animation-done")
+              ) {
+                $object.removeClass("white");
+              } else {
+                $object.addClass("white");
+              }
             } else {
-              logo.addClass("white");
-              menu.addClass("white");
+              $object.addClass("white");
             }
           } else {
-            logo.addClass("white");
-            menu.addClass("white");
+            $object.removeClass("white");
           }
         } else {
-          logo.removeClass("white");
-          menu.removeClass("white");
+          $object.removeClass("white");
         }
       } else {
-        logo.removeClass("white");
-        menu.removeClass("white");
+        $object.removeClass("white");
       }
+    })
+
+
+
+
+    // scroll btn
+    let scrollBtnOffset = $('.scroll-btn').offset().top
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() >= 200) {
+      $('.scroll-btn').fadeIn();
     } else {
-      logo.removeClass("white");
-      menu.removeClass("white");
+      $('.scroll-btn').fadeOut();
     }
+
+
   }
   function mediaChecker(max_min, resolution, width = "width") {
     return window.matchMedia(`(${max_min}-${width}: ${resolution}px)`).matches;
